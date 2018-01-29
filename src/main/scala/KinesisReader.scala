342ae3c3226d8b091420d2a62564d7daf5695384
Susie Coleman <susie.coleman@guardian.co.uk>
2018-01-29 09:45:27 +0000

    Start work on kinesis reader.

--- src/main/scala/KinesisReader.scala
+++ src/main/scala/KinesisReader.scala
@@ -1,69 +1,50 @@
-//import java.util.concurrent.Executors
-//
-//import com.amazonaws.services.kinesis.model.{GetRecordsRequest, GetShardIteratorRequest, Record}
-//import Config._
-//import com.amazonaws.auth.AWSCredentialsProvider
-//import com.amazonaws.services.kinesis.clientlibrary.lib.worker.Worker
-//import com.amazonaws.services.kinesis.metrics.impl.NullMetricsFactory
-//import com.google.common.util.concurrent.ThreadFactoryBuilder
-//
-//import collection.JavaConverters._
-//
-//trait KinesisStreamReader {
-//
-//  val streamName: String
-//  val stage: String
-//  val kinesisCredentialsProvider: AWSCredentialsProvider
-//
-//
-//  lazy val worker = new Worker(
-//    eventProcessorFactory,
-//    kinesisConfig,
-//    new NullMetricsFactory(), // don't send metrics to CloudWatch because it's expensive and not very helpful
-//    Executors.newCachedThreadPool(new ThreadFactoryBuilder().setNameFormat(s"${getClass.getSimpleName}-$workerId-thread-%d").build())
-//  )
-//
-//  /* Start the worker in a new thread. It will run forever */
-//  private lazy val workerThread =
-//    new Thread(worker, s"${getClass.getSimpleName}-$workerId")
-//
-//
-//  def start(): Unit = workerThread.start()
-//}
-//
-//object KinesisClient {
-//
-//  def getRecords(startingSequenceNumber: String): Seq[Record] = {
-//    try {
-//
-//      val shardId = client.describeStream(streamName).getStreamDescription.getShards.asScala.head.getShardId
-//
-//      val iteratorRequest = new GetShardIteratorRequest()
-//        .withStreamName(streamName)
-//        .withShardId(shardId)
-//        .withShardIteratorType("AT_SEQUENCE_NUMBER")
-//        .withStartingSequenceNumber(startingSequenceNumber)
-//
-//      var iterator = client.getShardIterator(iteratorRequest).getShardIterator
-//
-//      val request = new GetRecordsRequest().withShardIterator(iterator)
-//
-//      var list = Seq.empty[Record]
-//
-//      while(list.length < 10) {
-//        val result = client.getRecords(request)
-//        iterator = result.getNextShardIterator
-//        list = list ++ Seq(result.getRecords.asScala: _*)
-//        Thread.sleep(1000)
-//      }
-//      list
-//
-//    } catch {
-//      case e: Throwable =>
-//        println(e)
-//        Seq.empty[Record]
-//    }
-//
-//  }
-//
-//}
+import com.amazonaws.services.kinesis.model._
+import Config._
+
+import collection.JavaConverters._
+import scala.util.{Failure, Success, Try}
+
+
+implicit class RichTry[T](t: Try[T]) {
+  def convertTryToEither:Either[KinesisError, T] =
+    t match {
+      case Success(event) => Right(event)
+      case Failure(f: ResourceNotFoundException) => Left(KinesisStreamNotFoundError(f.getErrorMessage))
+      case Failure(f: ProvisionedThroughputExceededException) =>Left(ThroughputExceededError(f.getErrorMessage))
+      case Failure(f) => Left(KinesisGenericError(f.getLocalizedMessage))
+    }
+}
+
+object KinesisReader {
+
+  def readRecord(sequenceNumber: String)(implicit config: KinesisConfig): Either[KinesisError , Option[Record]] = {
+    Try {
+
+      val client = config.client
+      val streamName = config.streamName
+
+      val shardId = client.describeStream(streamName).getStreamDescription.getShards.asScala.head.getShardId
+
+      val iteratorRequest = new GetShardIteratorRequest()
+        .withStreamName(streamName)
+        .withShardId(shardId)
+        .withShardIteratorType("AT_SEQUENCE_NUMBER")
+        .withStartingSequenceNumber(sequenceNumber)
+
+      val iterator = client.getShardIterator(iteratorRequest).getShardIterator
+
+      val request = new GetRecordsRequest().withShardIterator(iterator)
+      client.getRecords(request).getRecords.asScala.headOption
+
+    } match {
+      case Success(event) => Right(event)
+      case Failure(f: ResourceNotFoundException) => Left(KinesisStreamNotFoundError(f.getErrorMessage))
+      case Failure(f: ProvisionedThroughputExceededException) =>Left(ThroughputExceededError(f.getErrorMessage))
+      case Failure(f) => Left(KinesisGenericError(f.getLocalizedMessage))
+    }
+  }
+
+
+
+}
+
